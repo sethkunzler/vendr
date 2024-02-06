f56af964da01762aac9b006375fe2dfc46665d16
Seth Kunzler <sethkunzler@gmail.com>
2024-02-06 15:21:20 -0700

    Initiated

--- app/AppState.js
+++ app/AppState.js
@@ -5,6 +5,14 @@ class ObservableAppState extends EventEmitter {
 
   /**@type {import('./models/Example.js').Example[]} */
   examples = []
+  
+  /**@type {import('./models/Example.js').Example[]} */
+  snacks = [
+    new Snack({name:'Sample', price: 3.50, imgUrl: "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=3160&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"})
+  
+  ]
+
+  money = 0 
 }
 
 export const AppState = createObservableProxy(new ObservableAppState())
---
+++ app/models/Snack.js
@@ -0,0 +1,7 @@
+export class Snack {
+  constructor(data)  {
+    this.name = data.name
+    this.price = data.price
+    this.imgUrl = data.imgUrl
+  }
+}
