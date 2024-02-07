762ec8a97038752e0b22d46810ed0fbf2cfa1c46
Seth Kunzler <sethkunzler@gmail.com>
2024-02-06 17:17:03 -0700

    going home

--- app/controllers/VendingController.js
+++ app/controllers/VendingController.js
@@ -1,11 +1,18 @@
 import { AppState } from "../AppState.js"
 import { vendingService } from "../services/VendingService.js"
+import { setText } from "../utils/Writer.js";
+
 
 export class VendingController {
   constructor() {
     console.log("Vending Controller Constructed")
+    AppState.on('money', this.#_drawMoney)
   }
   CollectMoney(data) {
     vendingService.MoneyGoesUp(data)
+  }
+
+  #_drawMoney() {
+    setText('moneyDisplay', AppState.money)
   }
 }
